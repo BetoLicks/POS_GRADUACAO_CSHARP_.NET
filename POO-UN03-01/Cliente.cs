9e2b771ea1c6f3a9800e443cc4700f7a4b07c714
Beto Licks <betaolicks@gmail.com>
2024-06-26 10:55:58 -0300

    feat: cria classe cliente

---
+++ POO-UN03-01/Cliente.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_UN03_01
+{
+    public class Cliente
+    {
+        public int codigo;
+        public string nome;
+        public decimal rendaMensal;
+        public bool clientePremium;
+    }
+}
---
+++ POO-UN03-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using POO_UN03_01;
+
+Cliente cliente1 = new Cliente();
+cliente1.codigo = 1;
+cliente1.nome = "Delson Reis";
+cliente1.rendaMensal = 2500;
+cliente1.clientePremium = false;
+
+Console.WriteLine("Cliente1: " + cliente1.nome);
+Console.ReadKey();
+
+
