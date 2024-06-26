e467ce8967b13adeb364e25fa49a2d1c33b4e150
Beto Licks <betaolicks@gmail.com>
2024-06-26 16:08:54 -0300

    fix: lista emails com array

--- POO-UN03-01/Cliente.cs
+++ POO-UN03-01/Cliente.cs
@@ -9,11 +9,12 @@ namespace POO_UN03_01
     public class Cliente{
         public const decimal RENDA_MINIMA = 2500;
         public const decimal VALOR_MINIMO = 5000;
-        public Cliente(int codigo, string nome, decimal rendamensal){
+        public Cliente(int codigo, string nome, decimal rendamensal, string[] emails){
            Codigo = codigo;
            Nome = nome;
            RendaMensal = rendamensal;
            ClientePremium = RendaMensal >= RENDA_MINIMA;
+           Emails = emails;
         }
 
         public int Codigo{get; private set;}
@@ -21,12 +22,21 @@ namespace POO_UN03_01
         public decimal RendaMensal{get;set;}
         public bool ClientePremium{get;set;}
         public bool ClienteMaster { get; set; }
+        public string[] Emails{get; set;}
         public bool PromoverCliente(decimal valor){
             if (valor >= VALOR_MINIMO){
                 ClientePremium = true;
                 return true;
             }
             return false;            
+        }
+
+        public string ListaEmails(){
+            var listaEmail = "O cliente possui " + Emails;
+            foreach(var email in Emails){
+                listaEmail += email + "\n";
+            }
+            return listaEmail;
         }
     }
 }
--- POO-UN03-01/Program.cs
+++ POO-UN03-01/Program.cs
@@ -1,7 +1,8 @@
 ﻿using POO_UN03_01;
 
-Cliente cliente1 = new Cliente(1, "João", 1200);
-Cliente cliente2 = new Cliente(2,"Maria",6000); 
+string[] emails  = new string[]{"joao@gmail.com","maria@gmail.com","jose@gmail.com"};
+Cliente cliente1 = new Cliente(1, "João", 1200, emails);
+Cliente cliente2 = new Cliente(2,"Maria",6000, emails); 
 
 Console.WriteLine("Cliente1: " + cliente1.Nome + cliente1.PromoverCliente(6000));
 Console.WriteLine("Cliente2: " + cliente2.Nome);
