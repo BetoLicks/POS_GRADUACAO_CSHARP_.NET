8af1e7c27b60c1463145647882a50b70c55a5a2b
Beto Licks <betaolicks@gmail.com>
2024-06-27 14:30:31 -0300

    fix: criação de métodos para herança

--- POO-UN04-03/Conta.cs
+++ POO-UN04-03/Conta.cs
@@ -7,6 +7,17 @@ namespace POO_UN04_03
 {
     public class Conta
     {
-        
+        public decimal Saldo {get; protected set;}
+        public void Depositar(decimal valor){
+            Saldo += valor;
+        }
+
+        public virtual bool Saque(decimal valor){
+            if (valor <= Saldo){
+                Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
     }
 }
--- POO-UN04-03/ContaCorrente.cs
+++ POO-UN04-03/ContaCorrente.cs
@@ -5,8 +5,15 @@ using System.Threading.Tasks;
 
 namespace POO_UN04_03
 {
-    public class ContaCorrente
+    public class ContaCorrente: Conta
     {
-        
+        public decimal Limite{get; private set;}
+        override public bool Saque(decimal valor){
+            if (valor <= (Saldo + Limite)){
+                Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
     }
 }
