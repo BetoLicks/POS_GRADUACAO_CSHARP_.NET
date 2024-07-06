c6a46532592384206b3b0edbe8465989367cdc39
Beto Licks <betaolicks@gmail.com>
2024-07-06 03:31:20 -0300

    feat: implementa excessões

--- POO-UN05-01/Program.cs
+++ POO-UN05-01/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_UN05_01
+{
+    public class Progrmama
+    {
+        static void Main(string[] args){
+         try
+         {
+            var valor = 10;
+            var retorna = valor / 0;
+         }
+         catch (Exception erro)
+         {
+            Console.WriteLine("ERROOOOOO: " + erro.Message);
+         }
+        }
+    }        
+}
