2c2a106e93f0b3a261bc06efe01979b0197ec676
Beto Licks <betaolicks@gmail.com>
2024-07-06 02:53:09 -0300

    fix: interfaces

---
+++ POO-UN04-03/BoletoBancodoSul.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_UN04_03
+{
+    class BoletoBancodoSul : iBoleto
+    {
+        public string GerarCodigoDeBarras(){
+            return "";
+        }
+        
+    }
+}
--- POO-UN04-03/obj/Debug/net8.0/POO-UN04-03.AssemblyInfo.cs
+++ POO-UN04-03/obj/Debug/net8.0/POO-UN04-03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO-UN04-03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0441a6bd3de8dfe991ffcb826b202a8e97ed607f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af1e7c27b60c1463145647882a50b70c55a5a2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO-UN04-03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO-UN04-03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
